bed6b6a6d72d28d7ed96aa6d1e3d8affe2dcde1f
theusindabike <mlopes.matheus@gmail.com>
2023-05-29 21:24:06 -0300

    initial

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/api/v1/transactions")
+async def root():
+    return {
+        "results": [
+            {
+                "id": "fake_id_1",
+                "user": "matheus",
+                "value": 111.11,
+                "category": "marketStuff",
+                "type": "even",
+                "description": "fake description 1",
+            },
+            {
+                "id": "fake_id_2",
+                "user": "matheus",
+                "value": 222.22,
+                "category": "marketStuff",
+                "type": "even",
+                "description": "fake description 2",
+            },
+            {
+                "id": "fake_id_3",
+                "user": "matheus",
+                "value": 3.33,
+                "category": "marketStuff",
+                "type": "even",
+                "description": "fake description 3",
+            },
+            {
+                "id": "fake_id_4",
+                "user": "bianca",
+                "value": 4444.44,
+                "category": "marketStuff",
+                "type": "even",
+                "description": "fake description 4",
+            },
+            {
+                "id": "fake_id_5",
+                "user": "bianca",                                                                   
+                "value": 5.55,                                                                   
+                "category": "marketStuff",                                                          
+                "type": "even",                                                                     
+                "description": "fake description 4"
+            },
+        ]
+    }
+
+
+@app.post("/")
+async def post_transaction():
+    return {"message": "Hello World"}
