6081806b1345766ad43addc2645e0be5960d2e49
theusindabike <mlopes.matheus@gmail.com>
2023-06-01 17:23:02 -0300

    feat: config db

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from src.api import main_router
+from src.db import create_db_and_tables, engine
 
 
 def start_application() -> FastAPI:
-    application = FastAPI()
+    application = FastAPI(title="Vinttem API")
     application.include_router(main_router.router, prefix="/api")
 
     return application
 
 
 app = start_application()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables(engine)
